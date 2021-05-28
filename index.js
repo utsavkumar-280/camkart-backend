735ad2b9dcddf5fce0ad4c6980eef71db4ec97ff
Utsav Kumar <utsavkumar280@gmail.com>
2021-05-28 17:37:41 +0530

    feat: created some routes

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const morgan = require("morgan");
-require('dotenv').config();
+require("dotenv").config();
 
+// const users = require("./routes/users.router");
+const products = require("./routes/products.router");
+// const carts = require("./routes/carts.router");
+// const wishlists = require("./routes/wishlists.router");
 
-const dbConnection = require('./db/dbConnect.js')
-const route404Handler = require('./middlewares/route404Handler');
-const errorHandler = require('./middlewares/errorHandler');
-
+const dbConnection = require("./db/dbConnect.js");
+const route404Handler = require("./middlewares/route404Handler");
+const errorHandler = require("./middlewares/errorHandler");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -20,18 +23,22 @@ app.use(cors());
 dbConnection();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to CamKart Apis");
+	res.send("Welcome to CamKart Apis");
 });
 app.get("/hello", (req, res) => {
-  res.json({
-    success: true,
-    message: "Hey, What's up?"
-  });
+	res.json({
+		message: "Kya bolti public?",
+	});
 });
+
+// app.use("/users", users);
+app.use("/products", products);
+// app.use("/wishlists", wishlists);
+// app.use("/carts", carts);
 
 app.use(route404Handler);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`server running on http://localhost:${PORT}`);
+	console.log(`server running on http://localhost:${PORT}`);
 });
