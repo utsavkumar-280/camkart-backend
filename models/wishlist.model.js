54fd4feff049b034ada1885f7f9a22a8b69a0bf4
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-22 14:46:30 +0530

    feat: added wishlist model

---
+++ models/wishlist.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const WishlistSchema = new Schema({
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+	products: [
+		{
+			product: {
+				type: Schema.Types.ObjectId,
+				ref: "Product",
+				quantity: Number,
+				isActive: Boolean,
+			},
+		},
+	],
+});
+
+const Wishlist = model("Wishlist", WishlistSchema);
+
+module.exports = { Wishlist };
