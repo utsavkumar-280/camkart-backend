e2b03e955c27a3c96d6177b4c163ee5ae3d6602d
Utsav Kumar <utsavkumar280@gmail.com>
2021-06-22 14:45:37 +0530

    feat: added address model

---
+++ models/address.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const AddressSchema = new Schema({
+	userId: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+	},
+	fullName: {
+		type: String,
+		required: "Full name is required",
+	},
+	buildingName: {
+		type: String,
+		required: "Building Name is required",
+	},
+	pincode: {
+		type: Number,
+		required: "Pincode is required",
+	},
+	city: {
+		type: String,
+		required: "City is required",
+	},
+	state: {
+		type: String,
+		required: "State is required",
+	},
+	phone: {
+		type: Number,
+		required: "Phone number is required",
+	},
+});
+
+const Address = model("Address", AddressSchema);
+
+module.exports = { Address };
